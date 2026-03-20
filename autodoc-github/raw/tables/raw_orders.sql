639773631839da9f1fdd65f78cf0cdc74f7329c1
Carlos Batista <cbatis20@ford.com>
2026-03-20 14:31:29 -0300

    ajuste tabela raw_orders

--- autodoc-github/raw/tables/raw_orders.sql
+++ autodoc-github/raw/tables/raw_orders.sql
@@ -3,6 +3,6 @@ CREATE TABLE raw_orders (
     customer_id VARCHAR(50),
     order_date_string VARCHAR(50), -- Data como string na camada raw
     total_amount_string VARCHAR(50), -- Valor total como string na camada raw
-    status VARCHAR(50),
+    status VARCHAR(100),
     load_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
