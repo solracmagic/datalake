3b4829d0d92eb7c79d567f0151577bf858450406
Carlos Batista <cbatis20@ford.com>
2026-03-23 21:37:15 -0300

    teste 2 github

--- autodoc-github/refined/tables/refined_daily_sales.sql
+++ autodoc-github/refined/tables/refined_daily_sales.sql
@@ -1,8 +1,8 @@
 CREATE TABLE refined_daily_sales AS
 SELECT
     order_date,
-    COUNT(DISTINCT order_id) AS number_of_orders_all,
-    SUM(total_amount) AS total_daily_sales,
+    COUNT(DISTINCT order_id) AS number_of_orders,
+    SUM(total_amount) AS total_daily_sales_batista,
     MAX(load_timestamp) AS latest_load_timestamp
 FROM
     trusted_orders
